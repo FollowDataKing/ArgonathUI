3048b0869d396619ebab62a570a29a0ac020044a
baihe <baihe@xiaomei.com>
2016-03-23 14:52:50 +0800

    TRIV-clean the code

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -7,9 +7,10 @@
       <!-- <img v-if="type == 'placeholder'" data-src="holder.js/100%x{{ height }}/white"> -->
       <Loading v-if="status == 'loading'"></Loading>
       <div v-else>
+        <!-- <path-nav :path="['Home', 'Library', 'Data']"></path-nav> -->
         <responsive-table
-          :dimension="data.dimension"
-          :measures="data.measures">
+          :columns="columns"
+          :records="data">
         </responsive-table>
         <canvas v-if="type == 'chart'" id="myChart"></canvas>
       </div>
@@ -24,6 +25,7 @@
 // import Chart from "chart.js"
 import Loading from "./particles/Loading.vue"
 import ResponsiveTable from "./particles/ResponsiveTable.vue"
+import PathNav from "./particles/PathNav.vue"
 
 export default {
   props: {
@@ -43,10 +45,13 @@ export default {
           TENPAY2: "微信2",
           TOTAL: "总额"
         },
+        // groupBy: ["create_time"],
+        // filters: ["create_time", "city_code", "shop_area_code", "shop_id"],
         dimension: "create_time",
         measures: [ "BALANCE", "ALIPAY", "TENPAY", "TENPAY2", "TOTAL" ]
       },
-      data: { }
+      data: { },
+      drilled: []
     }
   },
 
@@ -56,57 +61,84 @@ export default {
     },
     type: function() {
       return this.model.type
+    },
+    columns: function () {
+      var cols = []
+      cols.push({
+        key: this.scheme.dimension,
+        label: this.scheme.alias[this.scheme.dimension]
+      })
+
+      for (var idx in this.scheme.measures) {
+        var measure_key = this.scheme.measures[idx]
+        cols.push({
+          key: measure_key,
+          label: this.scheme.alias[measure_key]
+        })
+      }
+
+      return cols
     }
   },
 
   components: {
     Loading,
-    ResponsiveTable
+    ResponsiveTable,
+    PathNav
   },
 
   ready() {
     // GET request
-    this.$http({url: 'http://data4:1060/statistics/query/payment', method: 'GET'})
+    this.$http({url: this.model.api.url, method: 'GET'})
     .then(function (response) {
       // success callback
 
-      var data = {
-        dimension: {
-          label: this.scheme.alias[this.scheme.dimension],
-          data: []
-        },
-        measures: {}
-      }
+      // var data = {
+      //   dimension: {
+      //     label: this.scheme.alias[this.scheme.dimension],
+      //     data: []
+      //   },
+      //   measures: {}
+      // }
+      //
+      // for (var idx in this.scheme.measures) {
+      //   var measure_key = this.scheme.measures[idx]
+      //   data.measures[measure_key] = {
+      //     label: this.scheme.alias[measure_key],
+      //     data: []
+      //   }
+      // }
+      //
+      // for (var idx in response.data) {
+      //   var item = response.data[idx]
+      //
+      //   for (var key in item) {
+      //     if (key == this.scheme.dimension) {
+      //       data.dimension.data.push(item[key])
+      //     }
+      //     else {
+      //       data.measures[key].data.push(item[key])
+      //     }
+      //   }
+      // }
+      // this.data = data
 
-      for (var idx in this.scheme.measures) {
-        var measure_key = this.scheme.measures[idx]
-        data.measures[measure_key] = {
-          label: this.scheme.alias[measure_key],
-          data: []
-        }
-      }
-
-      for (var idx in response.data) {
-        var item = response.data[idx]
-
-        for (var key in item) {
-          if (key == this.scheme.dimension) {
-            data.dimension.data.push(item[key])
-          }
-          else {
-            data.measures[key].data.push(item[key])
-          }
-        }
-      }
-      this.data = data
-
+      this.data = response.data
       this.status = "ready"
     }, function (response) {
       // error callback
       this.status = "error"
       console.log(response)
     });
-  }
+  },
+
+  events: {
+    'dblclickedOnTableCell': function (column, row) {
+      if (column.key == this.scheme.dimension) {
+        console.log("dblclicked on dimension: " + column.key)
+      }
+    }
+  },
 }
 
 </script>
