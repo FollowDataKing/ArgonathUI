a4ae53e0833bec7e2fab4b13c5bfe00924b0938f
mokaikai <mokaikai@xiaomei.com>
2016-03-28 00:37:41 +0800

    修改目录结构,未修改原先引用，直接跑代码会有问题

---
+++ src/components/sidebar-user-panel.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Sidebar user panel -->
+  <div class="user-panel">
+    <div class="pull-left image">
+      <img :src="img" class="img-circle" alt="User Image">
+    </div>
+    <div class="pull-left info">
+      <p>Alexander Pierce</p>
+      <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      img: require("admin-lte/dist/img/user2-160x160.jpg")
+    }
+  }
+}
+</script>
